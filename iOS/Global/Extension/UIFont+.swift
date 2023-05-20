ba1407b0e726740907a40d5904a8857c51d4e5bd
HELLOHIDI <fbgmlwo123@naver.com>
2023-05-20 18:35:16 +0900

    [Add] 폰트 추가 #3

---
+++ iOS/Global/Extension/UIFont+.swift
@@ -0,0 +1,42 @@
+//
+//  UIFont+.swift
+//  iOS
+//
+//  Created by 류희재 on 2023/05/20.
+//
+
+import UIKit
+
+//MARK: - Custom Font
+
+extension UIFont{
+    
+    class var soptHeadLine1: UIFont {
+        return UIFont(name: "Pretendard-Bold", size: 20)!
+    }
+    
+    class var soptSubhead1: UIFont {
+        return UIFont(name: "Pretendard-Medium", size: 16)!
+    }
+    
+    class var soptTitle1: UIFont {
+        return UIFont(name: "Pretendard-Bold", size: 16)!
+    }
+    
+    class var soptTitle2: UIFont {
+        return UIFont(name: "Pretendard-Bold", size: 14)!
+    }
+   
+    class var soptBody1: UIFont {
+        return UIFont(name: "Pretendard-Bold", size: 15)!
+    }
+    
+    class var soptBody2: UIFont {
+        return UIFont(name: "Pretendard-Medium", size: 15)!
+    }
+    
+    class var soptBody3: UIFont {
+        return UIFont(name: "Pretendard-Medium", size: 15)!
+    }
+}
+
