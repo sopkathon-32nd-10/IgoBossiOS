2b0ada12a2d3570857a95e0bb35cfd8b21445d09
heejoo <heejoo_byun@naver.com>
2023-05-20 23:42:05 +0900

    [Feat] #11 - Check dummy 생성

--- iOS/Data/Check/Check.swift
+++ iOS/Data/Check/Check.swift
@@ -1,1 +1,24 @@
+//
+//  Check.swift
+//  iOS
+//
+//  Created by 변희주 on 2023/05/20.
+//
 
+import UIKit
+
+struct Check {
+    let checkBoxButton: String
+    let title: String
+    let content: String
+    let goWebsiteButton: String
+}
+
+extension Check {
+    static func dummy() -> [Check] {
+        return [Check(checkBoxButton: "버튼", title: "가나다라마바사아", content: "이건이렇고저건ㄱ저렇고셀을이렇게그렇게해서이렇게저렇게", goWebsiteButton: "자세히보기"),
+                Check(checkBoxButton: "버튼", title: "응응응앙아앙", content: "이건이렇고저건ㄱ저렇고셀을이렇게그렇게해서이렇게저렇게", goWebsiteButton: "자세히보기"),
+                Check(checkBoxButton: "버튼", title: "엉그래그래엉엉", content: "이건이렇고저건ㄱ저렇고셀을이렇게그렇게해서이렇게저렇게", goWebsiteButton: "자세히보기"),
+                Check(checkBoxButton: "버튼", title: "아누이ㅏ누이", content: "이건이렇고저건ㄱ저렇고셀을이렇게그렇게해서이렇게저렇게", goWebsiteButton: "자세히보기"),
+                Check(checkBoxButton: "버튼", title: "ㅎㅎㅎ헤헤헤", content: "이건이렇고저건ㄱ저렇고셀을이렇게그렇게해서이렇게저렇게", goWebsiteButton: "자세히보기")]}
+}
