7732a635533051d03665dd93538b1be1a0a47c89
heejoo <heejoo_byun@naver.com>
2023-05-20 23:41:29 +0900

    [Feat] #11 - ViewController에서 tableView 생성 및 설정

--- iOS/Presentation/Check/CheckViewController.swift
+++ iOS/Presentation/Check/CheckViewController.swift
@@ -7,12 +7,44 @@
 
 import UIKit
 
-class CheckViewController: BaseViewController {
+import SnapKit
+import Then
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view.
+final class CheckViewController: BaseViewController {
+    
+    private let dummy = Check.dummy()
+    
+    private let checkTableView = UITableView()
+    
+    override func setUI() {
+        view.backgroundColor = .white
+        checkTableView.do {
+            $0.separatorStyle = .none
+            $0.backgroundColor = .soptGrey200
+        }
+        
+    }
+    
+    override func setLayout() {
+        view.addSubviews(checkTableView)
+        checkTableView.snp.makeConstraints {
+            $0.top.equalTo(view.safeAreaInsets)
+            $0.leading.trailing.bottom.equalToSuperview()
+        }
     }
 
+}
 
+extension CheckViewController: UITableViewDelegate, UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return dummy.count
+
+    }
+
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        guard let cell = tableView.dequeueReusableCell(withIdentifier: CheckTableViewCell.identifier, for: indexPath) as? CheckTableViewCell else
+        {return UITableViewCell()}
+        
+        
+        return cell}
 }
