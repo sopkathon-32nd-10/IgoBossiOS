49583da7c1c2e8a450d1f2b4bdd7f0cafeca3f5f
GodSamble <osgneb8359@naver.com>
2023-05-21 01:33:32 +0900

    [Add] #17 - 마크주석 추가

--- iOS/Presentation/Home/View/HomeView.swift
+++ iOS/Presentation/Home/View/HomeView.swift
@@ -1,8 +1,23 @@
 //
-//  HomeView.swift
-//  iOS
+//  HomeViewController.swift
+//  iOS
 //
-//  Created by 고영민 on 2023/05/21.
+//  Created by 류희재 on 2023/05/20.
 //
+import UIKit
+import SnapKit
+import Then
 
-import Foundation
+final class HomeViewController: BaseViewController {
+    
+    private let rootView = HomeView()
+    
+    // MARK: - App Life Cycle
+    override func loadView() {
+        self.view = rootView
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
