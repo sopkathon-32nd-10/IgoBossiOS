8cf2f0a00e4ea26e80be883539f50154db6ad607
HELLOHIDI <fbgmlwo123@naver.com>
2023-05-20 18:23:14 +0900

    [Add] 컬러 추가 #3

--- iOS/Global/Extension/UIColor+.swift
+++ iOS/Global/Extension/UIColor+.swift
@@ -16,4 +16,27 @@ extension UIColor {
 
 //MARK: - Custom Color
 
-extension UIColor{ }
+extension UIColor{
+    class var soptGrey000 : UIColor {
+        return UIColor(r: 121, g: 121, b: 121)
+    }
+    class var soptGrey100 : UIColor {
+        return UIColor(r: 234, g: 234, b: 234)
+    }
+    class var soptGrey200 : UIColor {
+        return UIColor(r: 217, g: 217, b: 217)
+    }
+    class var soptGrey300 : UIColor {
+        return UIColor(r: 168, g: 168, b: 168)
+    }
+    class var soptGrey400 : UIColor {
+        return UIColor(r: 124, g: 124, b: 124)
+    }
+    class var soptGrey500 : UIColor {
+        return UIColor(r: 80, g: 80, b: 80)
+    }
+    class var soptGrey600 : UIColor {
+        return UIColor(r: 29, g: 29, b: 29)
+    }
+}
+
